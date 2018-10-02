92049de391a4dc6fbf83f4b2b259e72435dedb8e
CTSFSD <venkataramana.vemula@cognizant.com>
2018-10-02 19:52:10 +0530

    Initial commit

---
+++ src/app/menu/TasksController.js
@@ -0,0 +1,63 @@
+﻿(function () {
+
+    var TasksController = function ($scope, $filter, localTasksService) {
+        $scope.message = "List of Tasks";
+
+        $scope.Tasks = [];
+        $scope.Task = {};
+        $scope.editTask = null;
+
+        $scope.fetchTasks = function () {
+            $scope.Tasks = localTasksService.Tasks;
+        }
+
+        $scope.addTask = function (frmAddTask) {
+            if (frmAddTask.$valid) {
+                localTasksService.addTask($scope.Task);
+                $scope.Task = {}
+                $scope.fetchTasks();
+				alert("Task Added sucessfully");
+            }
+            else {
+                alert("Invalid values. All values are required");
+            }
+        }
+
+        $scope.updateTask = function (frmEditTask) {
+            if (frmEditTask.$valid) {
+                localTasksService.updateTask($scope.editTask);
+                $scope.editTask = null;
+                $scope.fetchTasks();
+				alert("Task Updated sucessfully");
+            }
+            else {
+                alert("Invalid values. All values are required");
+            }
+        }
+		$scope.endTask = function (frmEndTask) {
+            if (frmEditTask.$valid) {
+                localTasksService.endTask($scope.editTask);
+                $scope.editTask = null;
+                $scope.fetchTasks();
+				alert("Task Completed sucessfully");
+            }
+            else {
+                alert("Invalid values. All values are required");
+            }
+        }
+
+        $scope.setEditTask = function (selected) {
+            $scope.editTask = angular.copy(selected);
+            $scope.Task = {}
+        }
+
+        $scope.cancelEdit = function () {
+            $scope.editTask = null;
+            $scope.Task = {}
+        }
+
+        $scope.fetchTasks();
+    }
+
+    angular.module('TaskManagerApplication').controller('TasksController', ["$scope", "$filter", "localTasksService",  TasksController]);
+}());
---
+++ src/app/menu/localTasksService.js
@@ -0,0 +1,46 @@
+﻿(function () {
+
+    var localTasksService = function () {
+
+        // Just a temporary varibale to mimic auto increment on client side
+        var nextIdForTask = 6;
+
+ var _Tasks = [
+              { TaskID: 1, TaskName: "TaskName-1", ParentTask: "", Priority: "100", StartDate: "08/15/2018", EndDate: "10/15/2018" }, 
+			   { TaskID: 2, TaskName: "TaskName-2", ParentTask: "", Priority: "80", StartDate: "08/17/2018", EndDate: "10/28/2018"  },
+                { TaskID: 3, TaskName: "TaskName-3", ParentTask: "", Priority: "60", StartDate: "09/03/2018", EndDate: "11/15/2018"  },
+                { TaskID: 4, TaskName: "TaskName-4", ParentTask: "TaskName-2", Priority: "50", StartDate: "10/15/2018", EndDate: "12/15/2018"  },
+                { TaskID: 5, TaskName: "TaskName-5", ParentTask: "TaskName-3", Priority: "40", StartDate: "10/15/2018", EndDate: "11/15/2018"  }
+        ];
+        var _addTask = function (Task) {
+            Task.ID = nextIdForTask;
+            nextIdForTask += 1;
+            _Tasks.push(Task);
+        }
+
+        var _updateTask = function (Task) {
+           // A lame way of updating the Task in the list
+            for (var i = 0; i < _Tasks.length; ++i) {
+                if (_Tasks[i].ID == Task.ID) {
+                    _Tasks[i] = Task;
+                }
+            }
+        }
+		var _endTask = function (Task) {
+           // A lame way of updating the Task in the list
+            for (var i = 0; i < _Tasks.length; ++i) {
+                if (_Tasks[i].ID == Task.ID) {
+                    _Tasks[i] = Task;
+                }
+            }
+        }
+
+        return {
+            Tasks: _Tasks,
+            addTask: _addTask,
+            updateTask: _updateTask,
+			 endTask: _endTask
+        };
+    }
+ angular.module('TaskManagerApplication').factory('localTasksService', [localTasksService]);
+}());
